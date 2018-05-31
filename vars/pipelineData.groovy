ea45e47b2e8ccedb8143c446172d5b6307f516f8
jstuart <jstuart@redhat.com>
2018-05-31 10:50:13 -0500

    adding display

--- vars/pipelineData.groovy
+++ vars/pipelineData.groovy
@@ -14,7 +14,7 @@ def buildVars(String ciMessage) {
     buildVars['package'] = buildVars['fed_repo']
     buildVars['package_name'] = buildVars['fed_repo']
     buildVars['rpm_repo'] = "${env.WORKSPACE}/${buildVars['fed_repo']}_repo"
-    buildVars['displayName'] = buildVars['fed_repo']
+    buildVars['displayName'] = "Build #${env.BUILD_NUMBER} - Branch: ${buildVars['branch']} - Package: ${buildVars['fed_repo']}"
 
     return buildVars
 }
