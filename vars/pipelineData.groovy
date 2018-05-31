b2c3dedec70849cc1142cb94b8b406eaf8120719
jstuart <jstuart@redhat.com>
2018-05-31 14:35:35 -0500

    fixing prod vars

--- vars/pipelineData.groovy
+++ vars/pipelineData.groovy
@@ -7,18 +7,36 @@ def buildVars(String ciMessage) {
     def buildVars = [:]
 
     def branches = utils.setBuildBranch(message['request'][1])
-    buildVars['fed_repo'] = utils.repoFromRequest(message['request'][0])
-    buildVars['fed_branch'] = branches[1]
-    buildVars['fed_rev'] = "kojitask-${message['task_id']}"
-    buildVars['fed_task_id'] = message['task_id']
-    buildVars['PROVIDED_KOJI_TASKID'] = message['task_id']
-    buildVars['branch'] = branches[0]
-    buildVars['package'] = buildVars['fed_repo']
-    buildVars['package_name'] = buildVars['fed_repo']
-    buildVars['rpm_repo'] = "${env.WORKSPACE}/${buildVars['fed_repo']}_repo"
+    def fed_repo = utils.repoFromRequest(message['request'][0])
+    def stages = 
+                ["koji-build"                                     : [
+                    fed_branch                : branches[0]
+                    fed_repo                  : fed_repo,
+                    fed_rev                   : message['rev']
+                    rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo",
+            ],
+             "cloud-image-compose"                            : [
+                     rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo",
+                     package                  : fed_repo,
+                     branch                   : branches[1],
+                     fed_branch               : branches[0],
+
+             ],
+             "nvr-verify"                                     : [
+                     python3                  : "yes",
+                     rpm_repo                 : "/etc/yum.repos.d/${fed_repo}",
+             ],
+             "package-tests"                                   : [
+                     package                  : fed_repo,
+                     python3                  : "yes",
+                     TAG                      : "classic",
+                     branch                   : branches[1],
+                     build_pr_id              : (env.fed_pr_id) ?: ''
+             ]
+            ]
     buildVars['displayName'] = "Build #${env.BUILD_NUMBER} - Branch: ${buildVars['branch']} - Package: ${buildVars['fed_repo']}"
 
-    return buildVars
+    return stages
 }
 
 
