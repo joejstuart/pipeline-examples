c0d49166fb9d6d492d8d55c6309c4a4742af59c2
jstuart <jstuart@redhat.com>
2018-05-31 10:42:47 -0500

    adding jenkinsfile

--- vars/pipelineData.groovy
+++ vars/pipelineData.groovy
@@ -6,11 +6,11 @@ def buildVars(String ciMessage) {
     def message = readJSON text: ciMessage
     def buildVars = [:]
 
-    def (branch, fed_branch) = utils.setBuildBranch(message['request'][1])
+    def branches = = utils.setBuildBranch(message['request'][1])
     buildVars['fed_repo'] = utils.repoFromRequest(message['request'][0])
-    buildVars['fed_branch'] = fed_branch
+    buildVars['fed_branch'] = branches[1]
     buildVars['fed_rev'] = "kojitask-${message['task_id']}"
-    buildVars['branch'] = branch
+    buildVars['branch'] = branches[0]
     buildVars['package'] = buildVars['fed_repo']
     buildVars['package_name'] = buildVars['fed_repo']
     buildVars['rpm_repo'] = "${env.WORKSPACE}/${buildVars['fed_repo']}_repo"
