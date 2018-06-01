6556ba62a2055327283c9a20f021f7f55438601d
jstuart <jstuart@redhat.com>
2018-06-01 13:41:17 -0500

    removing closures

--- vars/pipelineData.groovy
+++ vars/pipelineData.groovy
@@ -30,57 +30,57 @@ def stageVars(String ciMessage) {
 
     def stages = [
             'koji-build':
-                    {
-                        [
-                                PROVIDED_KOJI_TASKID      : message['task_id'],
-                                fed_branch                : branches[1],
-                                fed_repo                  : fed_repo,
-                                fed_rev                   : message['rev'],
-                                rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo"
 
-                        ]
-                    },
+                    [
+                            PROVIDED_KOJI_TASKID      : message['task_id'],
+                            fed_branch                : branches[1],
+                            fed_repo                  : fed_repo,
+                            fed_rev                   : message['rev'],
+                            rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo"
+
+                    ],
+
             'repoquery':
-                    {
-                        [
-                                fed_branch                : branches[1],
-                                fed_repo                  : fed_repo,
-                                fed_rev                   : message['rev'],
-                                rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo"
 
-                        ]
+                    [
+                            fed_branch                : branches[1],
+                            fed_repo                  : fed_repo,
+                            fed_rev                   : message['rev'],
+                            rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo"
 
-                    },
+                    ],
+
+
             'cloud-image-compose':
-                    {
-                        [
-                                rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo",
-                                package                  : fed_repo,
-                                branch                   : branches[0],
-                                fed_branch               : branches[1]
-                        ]
-                    },
+
+                    [
+                            rpm_repo                  : "${env.WORKSPACE}/${fed_repo}_repo",
+                            package                  : fed_repo,
+                            branch                   : branches[0],
+                            fed_branch               : branches[1]
+                    ],
+
             'nvr-verify':
-                    {
-                        [
-                                python3                  : "yes",
-                                rpm_repo                 : "/etc/yum.repos.d/${fed_repo}",
-                                TEST_SUBJECTS            : "${env.WORKSPACE}/images/test_subject.qcow2"
 
-                        ]
-                    },
+                    [
+                            python3                  : "yes",
+                            rpm_repo                 : "/etc/yum.repos.d/${fed_repo}",
+                            TEST_SUBJECTS            : "${env.WORKSPACE}/images/test_subject.qcow2"
+
+                    ],
+
             'package-tests':
-                    {
-                        [
-                                package                  : fed_repo,
-                                python3                  : "yes",
-                                TAG                      : "classic",
-                                branch                   : branches[1],
-                                build_pr_id              : (env.fed_pr_id) ?: '',
-                                TEST_SUBJECTS            : "${env.WORKSPACE}/images/test_subject.qcow2"
 
-                        ]
-                    }
+                    [
+                            package                  : fed_repo,
+                            python3                  : "yes",
+                            TAG                      : "classic",
+                            branch                   : branches[1],
+                            build_pr_id              : (env.fed_pr_id) ?: '',
+                            TEST_SUBJECTS            : "${env.WORKSPACE}/images/test_subject.qcow2"
+
+                    ]
+
     ]
 
     return stages
